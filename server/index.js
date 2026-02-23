f651d56309827c674041c285346c5f3e500e2e3f
KUMARI GUDIYA <kumarigudiyaa03@gmail.com>
2026-02-23 18:35:39 +0530

    fix cors

--- server/index.js
+++ server/index.js
@@ -12,7 +12,10 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "https://career-connect-nu.vercel.app"
+  ],
   credentials: true
 }));
 app.use("/uploads", express.static("uploads"));
