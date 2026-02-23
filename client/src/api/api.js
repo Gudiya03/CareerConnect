8ed041914f347e5030088262a81ab5644fbcb011
KUMARI GUDIYA <kumarigudiyaa03@gmail.com>
2026-02-23 18:19:34 +0530

    connected frontend to deployed backend

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://careerconnect-backend-tchr.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
