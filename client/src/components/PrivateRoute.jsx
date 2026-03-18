5451581f7baabda0bfa399f45b80e7440143d316
KUMARI GUDIYA <kumarigudiyaa03@gmail.com>
2026-03-18 23:45:52 +0530

    updated project features

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem("accessToken"); // ✅ FIXED
 
   if (!token) {
     return <Navigate to="/login" replace />;
